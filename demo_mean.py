c02828a98aa5f1d8c773815d248159bac815ff50
marlonbetz <marlon.betz@gmx.de>
2017-04-22 17:43:33 +0200

    new model

---
+++ demo_mean.py
@@ -0,0 +1,24 @@
+import os
+import riseml
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import  matplotlib.pyplot as plt
+
+def predict(input_image):
+    input_image = Image.open(input_image).convert('RGB')
+    image = np.asarray(input_image, dtype=np.float32)
+
+    image = image.transpose(2, 0, 1)
+    image = np.tile(np.mean(image,axis=0),reps=(3,1,1))
+    image = image.reshape((1,) + image.shape)
+
+    result = np.uint8(image[0].transpose((1, 2, 0)))
+
+    med = Image.fromarray(result)
+
+    output_image = BytesIO()
+    med.save(output_image, format='JPEG')
+    return output_image.getvalue()
+
+riseml.serve(predict, port=os.environ.get('PORT'))

78bdb3dfa336ff3c6f3280d61fd1bc7d9b7c183a
marlonbetz <marlon.betz@gmx.de>
2017-04-22 20:31:37 +0200

    feed forward ae

--- demo_predict_ae.py
+++ demo_predict_ae.py
@@ -6,8 +6,8 @@ import numpy as np
 import tensorflow as tf
 
 
-height = 512
-width = 512
+height = 128
+width = 128
 dim_hidden = 1024
 X  = tf.placeholder(dtype=tf.float32, shape=[None,height*width])
 
@@ -22,11 +22,11 @@ b1 = tf.get_variable(initializer=tf.contrib.layers.xavier_initializer(),
 
 w2 = tf.get_variable(initializer=tf.contrib.layers.xavier_initializer(),
                         shape=[dim_hidden,height*width],
-                        name="w1")
+                        name="w2")
 
 b2 = tf.get_variable(initializer=tf.contrib.layers.xavier_initializer(),
                         shape=[height*width],
-                        name="b1")
+                        name="b2")
 
 Y_logits =tf.matmul( tf.nn.relu(tf.matmul(X,w1)+b1),w2)+b2
 
@@ -52,16 +52,19 @@ with tf.Session() as sess:
 
         image = np.asarray(input_image, dtype=np.float32)
 
-        image = image.transpose(2, 0, 1).reshape(1,-1)
-        image = np.mean(image,axis=0)
+        image = image.transpose(2, 0, 1)
+        image = np.mean(image,axis=0).reshape((1,-1))
+        print(image.shape)
 
-        pred_raw = sess.run(Y_pred,feed_dict={X:image})[0]
-        result = np.vstack((pred_raw,pred_raw,pred_raw))
+        pred_raw = sess.run(Y_pred,feed_dict={X:image})[0].reshape((height,width))
+        result = np.stack((pred_raw,pred_raw,pred_raw),axis=0)
+
+        print(result.shape)
 
 
-        result = np.uint8(result.transpose((1, 2, 0)))
+        result = result.transpose((1, 2, 0))
 
-        med = Image.fromarray(result)
+        med = Image.fromarray(np.uint8(result))
 
         output_image = BytesIO()
         med.save(output_image, format='JPEG')
